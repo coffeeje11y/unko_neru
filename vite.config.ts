93c8f64f03ba0931b92890e6bf650b1fc2757941
r1gel0629 <h2530120@gl.cc.uec.ac.jp>
2025-11-13 23:40:49 +0900

    名称修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: 'unko_morasu_neru',
+  base: 'unko_neru',
 })
